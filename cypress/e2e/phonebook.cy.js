eac87823d7bf44a04c8ebc2cafab9bf550abe68f
Your Name <you@example.com>
2025-06-16 22:33:05 +0300

    final tests

--- cypress/e2e/phonebook.cy.js
+++ cypress/e2e/phonebook.cy.js
@@ -5,8 +5,8 @@ describe('Phonebook e2e tests', function () {
     	name: 'Temporary Person',
     	number: '040-00000000',
     }
-	
-    //checks if Temporary Person exists and creates one if not
+
+		//checks if Temporary Person exists and creates one if not
 		cy.request('GET', 'https://phonebook-jgkw.onrender.com/api/persons').then(
 			(response) => {
 				expect(response.status).to.eq(200)
