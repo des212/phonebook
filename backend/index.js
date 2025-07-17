8a183a8a0e9a2a85987b214ff3209950be6b99a3
Your Name <you@example.com>
2025-07-17 22:18:09 +0300

    backend express static directory change for frontend

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(requestLogger)
-app.use(express.static('dist'))
+app.use(express.static('../frontend/dist'))
 
 /*let persons = [
 	{
