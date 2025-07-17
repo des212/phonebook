3afa63b3806d99b3456b6ff056b61915b674b266
Your Name <you@example.com>
2025-07-17 19:56:37 +0300

    test fixes

--- frontend/src/services/Persons.jsx
+++ frontend/src/services/Persons.jsx
@@ -1,25 +1,25 @@
 import axios from 'axios'
-const baseUrl = 'https://phonebook-jgkw.onrender.com/api/persons'
-//const baseUrl = 'http://localhost:3001/api/persons'
+
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001'
 
 const getAll = () => {
-	const request = axios.get(baseUrl)
+	const request = axios.get(`${VITE_BACKEND_URL}/api/persons`)
 	return request.then(response => response.data)
 }
 
 const create = newObject => {
-	const request = axios.post(baseUrl, newObject)
+	const request = axios.post(`${VITE_BACKEND_URL}/api/persons`, newObject)
 	return request.then(response => response.data)
 }
 
 const del = (id) => {
 	console.log(`Deleting person ${id}`)
-	const request = axios.delete(`${baseUrl}/${id}`)
+	const request = axios.delete(`${VITE_BACKEND_URL}/api/persons/${id}`)
 	return request.then(response => response.data)
 }
 
 const update = (id, updateObject) => {
-	const request = axios.put(`${baseUrl}/${id}`, updateObject)
+	const request = axios.put(`${VITE_BACKEND_URL}/api/persons/${id}`, updateObject)
 	return request.then(response => response.data)
 }
 
