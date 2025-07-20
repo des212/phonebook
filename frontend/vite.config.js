863a4c348fa1c70dfc1acca9b481ece4b0ee73d7
Your Name <you@example.com>
2025-07-20 19:53:22 +0300

    added support for docker containers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	server: {
-		host: '127.0.0.1',
-	},
 	plugins: [react()],
 	test: {
 		environment: 'jsdom',
 		globals: true,
 		setupFiles: './viteTestSetup.js',
-	}
+	},
+  	server: {
+		host: '127.0.0.1',
+    	allowedHosts: true
+  	}
 })
